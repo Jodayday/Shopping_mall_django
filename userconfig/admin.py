40add215f95d8e1ca496d904e5c7c92f1430d4d0
joday <iddqfd5@naver.com>
2022-03-19 13:33:55 +0900

    유저의 회원가입 간단하게 구현

---
+++ userconfig/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+# Register your models here.
+from .models import UserInfo
+
+
+class UserAdmin(admin.ModelAdmin):
+    "관리자 페이지에 유저모델 등록 클래스"
+    list_display = ('user_name', 'user_email', 'password')
+    "관리자 페이지에서 추가적인 정보 표시"
+
+
+admin.site.register(UserInfo, UserAdmin)
---
+++ userconfig/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+
+from django.contrib.auth.hashers import make_password, check_password
+# 패스워드 생성과 체크
+# Create your views here.
+from .models import UserInfo
+
+
+def register(request):
+    """
+    로그인 페이지에 2가지 요청이 들어옴 
+    링크타고 들어온것과 값을 작성해서 전송한것
+    """
+    print(request.POST.get)
+    if request.method == "GET":
+        return render(request, "userconfig/register.html")
+    elif request.method == "POST":
+        username = request.POST.get("username", None)
+        # get으로 가져옴 key값을 찾고 없으면 None 반환
+        useremail = request.POST.get("useremail", None)
+        password = request.POST.get("password", None)
+        repassword = request.POST.get("repassword", None)
+        # 입력받은 값을 가져옴 form의 input의 name명임
+
+        message = {}
+        # 필요한 추가정보 전달
+        if username and password and repassword and useremail:
+            "모두 입력 했을때"
+            if password == repassword:
+                "비번이 같을때"
+                model = UserInfo(
+                    user_name=username,
+                    user_email=useremail,
+                    password=make_password(password),
+                )
+                # 객체 생성
+                model.save()
+                # 객체를 모델에 저장
+            else:
+                message['error'] = "비밀번호가 서로 다릅니다."
+        else:
+            message['error'] = "값을 입력해주세요"
+
+        return render(request, "userconfig/register.html", message)
